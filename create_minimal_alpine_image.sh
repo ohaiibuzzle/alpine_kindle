66215f2ad1b54b5c63f0da342d907f99724026b7
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-08-12 00:20:52 +0700

    added: use existing minirootfs and actions

---
+++ create_minimal_alpine_image.sh
@@ -0,0 +1,46 @@
+#/usr/bin/env bash
+
+# Ensure we are root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+REPO="http://dl-cdn.alpinelinux.org/alpine"
+MOUNT_POINT="/mnt/alpine"
+IMAGE="./alpine.ext4"
+IMAGESIZE=2048 # Megabytes
+
+if [ -n "$1" ]; then
+  ARCH="$1"
+else
+  echo "Usage: $0 <architecture>"
+  echo "<architecture> is any architecture that Alpine Linux supports"
+  exit 1
+fi
+
+# Grab the prebuilt minirootfs
+wget "$REPO/latest-stable/releases/$ARCH/latest-releases.yaml"
+MINIROOTFS_FILE=$(grep "file: alpine-minirootfs-.*-$ARCH.tar.gz" latest-releases.yaml | awk '{print $2}')
+wget "$REPO/latest-stable/releases/$ARCH/$MINIROOTFS_FILE" -O minirootfs.tar.gz
+
+# Prepare the disk image
+dd if=/dev/zero of="$IMAGE" bs=1M count="$IMAGESIZE"
+mkfs.ext4 -F "$IMAGE"
+tune2fs -i 0 -c 0 -O ^has_journal "$IMAGE"
+
+# Mount the image
+mkdir -p "$MOUNT_POINT"
+mount -o loop "$IMAGE" "$MOUNT_POINT"
+
+# Extract the minirootfs
+tar -xzvf minirootfs.tar.gz -C "$MOUNT_POINT"
+
+# Preconfig the image
+echo "kindle" > "$MOUNT_POINT/etc/hostname"
+echo "nameserver 8.8.8.8" > "$MOUNT_POINT/etc/resolv.conf"
+
+# Unmount the image
+sync
+umount "$MOUNT_POINT"
+rm -rf "$MOUNT_POINT"
